b3028d6a971260683dee328fb9fc3b2ba6cf46c9
sharmachait <chait8126@gmail.com>
2025-10-17 21:45:52 +0530

    Decode bencoded strings #ns2

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,25 @@ use std::env;
 
 // Available if you need it!
 // use serde_bencode
-
-#[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+
+    let split = encoded_value.split_once(':');
+
+    match split {
+        Some((len,string)) => {
+            let length= len.parse::<usize>();
+            match length {
+                Ok(parsedLength) => {
+                    let encoded = &(string[..parsedLength]);
+                    serde_json::Value::String(encoded.to_string())
+                },
+                Err(e)=>{
+                    println!("{}", e);
+                    panic!("Unhandled encoded value: {}", encoded_value);
+                }
+            }
+        },
+        None => panic!("not encoded properly: {}", encoded_value)
     }
 }
 
@@ -25,13 +31,10 @@ fn main() {
     let command = &args[1];
 
     if command == "decode" {
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        eprintln!("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
-        // let encoded_value = &args[2];
-        // let decoded_value = decode_bencoded_value(encoded_value);
-        // println!("{}", decoded_value.to_string());
+        let encoded_value = &args[2];
+        let decoded_value = decode_bencoded_value(encoded_value);
+        println!("{}", decoded_value.to_string());
     } else {
         println!("unknown command: {}", args[1])
     }
